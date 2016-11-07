6cf97290ef3e12f5c53b990946c051c16b699f6e
Corey Young <cmyoung0718@gmail.com>
2016-11-07 15:50:05 -0800

    add allowed host

--- classproject/settings.py
+++ classproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','peaceful-badlands-42479.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','secure-bayou-76149.herokuapp.com']
 
 # Application definition
 
